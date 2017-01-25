c7a283710382b6e583bfb8abc7296537f7692f30
Kelvin Tan <kelvin.tyb@gmail.com>
2017-01-25 19:24:36 +0800

    added paperclip

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,5 +6,10 @@ class TagsController < ApplicationController
     @tags = Tag.all
   end
   def destroy
+    @tag = Tag.find(params[:id])
+    flash.notice = "Tag '#{@tag.name}' was destroyed!"
+
+    @tag.destroy
+    redirect_to tags_path
   end
 end
